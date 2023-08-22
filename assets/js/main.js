e2a1ef52d0fd81584a482a18dd76eac28b8e65c8
Nouran-Ebrahim <nouran.ssp@gmail.com>
2023-08-22 14:03:17 +0300

    edits2

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,22 +184,7 @@
     selector: ".glightbox",
   });
 
-  /**
-   * Skills animation
-   */
-  let skilsContent = select(".skills-content");
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: "80%",
-      handler: function (direction) {
-        let progress = select(".progress .progress-bar", true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute("aria-valuenow") + "%";
-        });
-      },
-    });
-  }
+
 
   /**
    * Porfolio isotope and filter
